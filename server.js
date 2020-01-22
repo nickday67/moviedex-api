3c0508833b75d7c7d41ba0383d1c6e25ce40fe2b
Nick Day <thenickday@icloud.com>
2020-01-22 14:51:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIES = require("./movies-data-small.json");
+
+//Initialize the main app
+const app = express();
+
+//Middlewares
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
+app.use(cors());
+app.use(helmet());
+
+app.use(function validateBearerToken(req, res, next) {
+  //Used process.env to hide UUID token code
+  const apiToken = process.env.API_TOKEN;
+  //Check whether the auth Token matches the UUID token code
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = MOVIES;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      //take the Movie response string and turn word to lowercase for case sensitivity
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
